cf791e6a18617da94f1212487d4a04ac57804c00
noahKim <noah.kim@osckorea.com>
2021-07-19 15:43:39 +0900

    init Add

---
+++ src/main/java/com/example/webdemoapi/WebDemoApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.webdemoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDemoApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WebDemoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webdemoapi/WebDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdemoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
