72142af4197b9e9206688a22a233b3ea480e0cdc
noahKim <noah.kim@osckorea.com>
2021-07-20 15:07:26 +0900

    ip address

--- src/main/java/com/example/webdemoapi/WebDemoApiApplication.java
+++ src/main/java/com/example/webdemoapi/WebDemoApiApplication.java
@@ -3,8 +3,10 @@ package com.example.webdemoapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.servlet.ServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import com.example.webdemoapi.RequestMain;
 
 @SpringBootApplication
 public class WebDemoApiApplication {
@@ -17,7 +19,8 @@ public class WebDemoApiApplication {
         try {
             local = InetAddress.getLocalHost();
             String ip = local.getHostAddress();
-            System.out.println("local ip : "+ip);
+            System.out.println("CURRENT LOCAL ip:port = "+ ip);
+
         } catch (UnknownHostException e1) {
             e1.printStackTrace();
         }
