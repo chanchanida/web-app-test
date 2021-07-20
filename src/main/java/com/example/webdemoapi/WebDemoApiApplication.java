4b564235c6957ef888fa4c64418bc53d91229d4f
noahKim <noah.kim@osckorea.com>
2021-07-20 14:47:39 +0900

    add ip source

--- src/main/java/com/example/webdemoapi/WebDemoApiApplication.java
+++ src/main/java/com/example/webdemoapi/WebDemoApiApplication.java
@@ -3,12 +3,24 @@ package com.example.webdemoapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class WebDemoApiApplication {
 
     public static void main(String[] args) {
 
         SpringApplication.run(WebDemoApiApplication.class, args);
+
+        InetAddress local;
+        try {
+            local = InetAddress.getLocalHost();
+            String ip = local.getHostAddress();
+            System.out.println("local ip : "+ip);
+        } catch (UnknownHostException e1) {
+            e1.printStackTrace();
+        }
     }
 
 }
